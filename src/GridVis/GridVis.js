5b1681013b4b6bed25fc4502dfd4fc30b61abd2d
Marcin Skotniczny <marcin@swmansion.com>
2018-06-05 23:01:41 +0200

    Some problems appear

--- src/GridVis/GridVis.js
+++ src/GridVis/GridVis.js
@@ -1,100 +1,82 @@
 import React, { Component } from 'react';
 import Cube from "../elements/Cube.js";
-import React3 from 'react-three-renderer';
-import * as THREE from 'three';
 
-const cube = (<boxGeometry
-  width={1}
-  height={1}
-  depth={1}
-/>)
+import "./GridVis.css";
 
-function GridElement({ element, rotation }) {
+
+function GridElement2D({element, onClick}) {
   return (
-    <mesh rotation={rotation}>
-      {cube}    
-      <meshBasicMaterial
-        color={0x007700}
-      />
-    </mesh>
-  )
+    <div className="GridElement2D"
+      style={{
+        top: `${100*element.p1[1]}%`,
+        left: `${100*element.p1[0]}%`,
+        right: `${100-100*element.p2[0]}%`,
+        bottom: `${100-100*element.p2[1]}%`,
+      }}
+      onClick={onClick}
+    />
+  );
+
 }
+
+
+function GridElement3D({element, onClick}) {
+  const { p1, p2 } = element;
+  return (
+    <div className="GridElement3D"
+      style={{
+        transform: `scale3d(${p2[0]-p1[0]}, ${p2[1]-p1[1]}, ${p2[2]-p1[2]})`,  
+        
+      }}
+      onClick={() => {console.log("DUPA")}}
+    > 
+      <div className="GridElement3D-scaler"
+        style={{
+          transform: `translate3d(${p1[0]/(p2[0]-p1[0])}em, ${p1[1]/(p2[1]-p1[1])}em, ${-p1[2]/(p2[2]-p1[2])}em)`, 
+        }}>
+        <div className="cube">
+          <div className="side front" />
+          <div className="side back" />
+          <div className="side top" />
+          <div className="side bottom" />
+          <div className="side left" />
+          <div className="side right" />
+        </div>
+      </div>
+    </div>
+  );
+
+}
+
+
 
 class GridVis extends Component {
   
   constructor(props, context) {
     super(props, context);
-    this._elements = [new Cube([0,0,0], [0.5,0.5,0.5]), new Cube([0.5,0.5,0.5], [1,1,1])];
-
-    this.state = {
-      cameraPosition: new THREE.Vector3(0, 0, 1000),
-      cameraRotation: new THREE.Euler(),
-      mouseInput: null,
-      hovering: false,
-      dragging: false,
-    };
-
-    this._cursor = {
-      hovering: false,
-      dragging: false,
-    };
-
-    this.lightPosition = new THREE.Vector3(0, 500, 2000);
-    this.lightTarget = new THREE.Vector3(0, 0, 0);
+    this.els = [new Cube([0,0,0], [0.5, 0.5, 0.5]), new Cube([0.5, 0.5, 0.5], [1, 1, 1]),
+      new Cube([0.5, 0.5, 0], [1, 1, 0.5])];
   }
   
+  handleClickElementByIdx(idx) {
+    return () => {
+      this.els
 
-  _onAnimate = () => {
-    this._onAnimateInternal();
-  };
-
-
-  componentDidMount() {
-
-    const {
-      container,
-      camera,
-    } = this.refs;
-
-    // const controls = new TrackballControls(camera);
-
-    // controls.rotateSpeed = 1.0;
-    // controls.zoomSpeed = 1.2;
-    // controls.panSpeed = 0.8;
-    // controls.noZoom = false;
-    // controls.noPan = false;
-    // controls.staticMoving = true;
-    // controls.dynamicDampingFactor = 0.3;
-
-    // this.controls = controls;
-
-    // this.controls.addEventListener('change', this._onTrackballChange);
+    }
   }
 
   render() {
-    const { width, height } = this.props;
-    const { rotation } = this.state;
-
-    console.log("R");
+    // const { width, height } = this.props;
+    // const { rotation } = this.state;
+    const GridElement = this.els[0].dims===2 ? GridElement2D : GridElement3D;
     return (
-      <React3 
-        mainCamera="camera"
-        width={width}
-        height={height}
-        onAnimate={this._onAnimate}
-        >
-        <scene>
-          <perspectiveCamera
-            name="camera"
-            fov={75}
-            aspect={width / height}
-            near={0.1}
-            far={1000}
-            position={this.cameraPosition}
-          />
-          <GridElement element={this._elements[0]} rotation={rotation}/>
-        </scene>
-      </React3>
+      <div className="GridVis">
+        <div className="GridVis-plane">
+        {this.els.map((it, idx) => (
+          <GridElement element={it} key={idx} onClick={this.handleClickElementByIdx(idx)}/>
+        ))}
+        </div>
+      </div>
     );
   }
 }
